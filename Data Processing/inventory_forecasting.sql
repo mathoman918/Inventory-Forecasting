7257b9112c30b0966cdc633fe27628abb169eae7
Nyandano Mathoma <119676814+mathoman918@users.noreply.github.com>
2026-04-10 12:14:23 +0200

    Update inventory_forecasting.sql

--- Data Processing/inventory_forecasting.sql
+++ Data Processing/inventory_forecasting.sql
@@ -47,19 +47,19 @@ FROM inventory_forecasting;
 --- Sales by Category
 SELECT
     Category,
-    SUM(`Units Sold`) AS Total_Units_Sold,
-    SUM(`Units Sold` * Price) AS Total_Revenue,
-    AVG(`Inventory Level`) AS Avg_Inventory
-FROM inventory_forecasting
+    SUM(Units_Sold) AS Total_Units_Sold,
+    SUM(Units_Sold * Price) AS Revenue,
+    AVG(Inventory_Level) AS Avg_Inventory
+FROM workspace.default.inventory_forecasting_clean
 GROUP BY Category
 ORDER BY Total_Units_Sold DESC;
 
 --- Sales by Region
 SELECT
     Region,
-    SUM(`Units Sold`) AS Total_Sales,
-    SUM(`Units Sold` * Price) AS Revenue
-FROM inventory_forecasting
+    SUM(Units_Sold) AS Total_Sales,
+    SUM(Units_Sold * Price) AS Revenue
+FROM workspace.default.inventory_forecasting_clean
 GROUP BY Region
 ORDER BY Revenue DESC;
 
@@ -67,31 +67,51 @@ ORDER BY Revenue DESC;
 SELECT
     YEAR(Date) AS Year,
     MONTH(Date) AS Month,
-    SUM(`Units Sold`) AS Monthly_Sales
-FROM inventory_forecasting
+    SUM(Units_Sold) AS Monthly_Sales
+FROM workspace.default.inventory_forecasting_clean
 GROUP BY YEAR(Date), MONTH(Date)
 ORDER BY Year, Month;
 
 --- 5. Promotion Analysis
 SELECT
     `Holiday/Promotion`,
-    AVG(`Units Sold`) AS Avg_Sales,
+    AVG(Units_Sold) AS Avg_Sales,
     AVG(Discount) AS Avg_Discount,
-    SUM(`Units Sold` * Price) AS Revenue
-FROM inventory_forecasting
+    SUM(Units_Sold * Price) AS Revenue
+FROM workspace.default.inventory_forecasting_clean
 GROUP BY `Holiday/Promotion`;
 
 --- 6. Forecast Accuracy
 SELECT
     Category,
-    AVG(ABS(`Units Sold` - `Demand Forecast`)) AS MAE
-FROM inventory_forecasting
+    AVG(ABS(Units_Sold - Demand_Forecast)) AS MAE
+FROM workspace.default.inventory_forecasting_clean
 GROUP BY Category;
 
 --- 7. Inventory & Stockout Analysis
 SELECT
     Category,
     COUNT(*) AS Records,
-    SUM(CASE WHEN `Inventory Level` < `Demand Forecast` THEN 1 ELSE 0 END) AS Stockout_Risk_Count
-FROM inventory_forecasting
+    SUM(CASE WHEN Inventory_Level < Demand_Forecast THEN 1 ELSE 0 END) AS Stockout_Risk_Count
+FROM workspace.default.inventory_forecasting_clean
 GROUP BY Category;
+
+--- 8. Sales by season
+SELECT
+    Weather_condition,
+    AVG(Units_Sold) AS AVG_Units_sold
+FROM workspace.default.inventory_forecasting_clean
+GROUP BY Weather_condition;
+
+--- 9. Top Products
+SELECT
+    Product_ID,
+    SUM(Units_Sold) AS Revenue
+FROM workspace.default.inventory_forecasting_clean
+GROUP BY Product_ID 
+ORDER BY Revenue DESC;
+
+--- preview the cleaned dataset
+SELECT *
+FROM workspace.default.inventory_forecasting_clean
+limit 10;
